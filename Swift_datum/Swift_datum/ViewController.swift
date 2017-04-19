2689ed5265724a2b3ff7e3e20f68217f7d54ecb8
tjakopec <tjakopec@gmail.com>
2017-04-19 09:56:15 +0200

    Šminkanje koda

--- Swift_datum/Swift_datum/ViewController.swift
+++ Swift_datum/Swift_datum/ViewController.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Tomislav Jakopec on 11.04.2017..
 //  Copyright © 2017. Tomislav Jakopec. All rights reserved.
+//  Kod nije najljepši, sklepan je da bude funkcionalan i odradi posao
 //
 
 import Cocoa
@@ -18,68 +19,61 @@ class ViewController: NSViewController {
     
     var nazivVremenskeZone: String { return TimeZone.current.identifier }
     
+    var server: String {return "http://it.ffos.hr/fz2017/"}
+    
+    var formatDatumaJSON: String {return "datumISO8601" }
+    
+    
+    
+    
+    
+    
+    //https://developer.apple.com/swift/blog/?id=37
     @IBAction func dohvati(_ sender: Any) {
-        //print("dohvaćam")
-        
-        let url = "http://it.ffos.hr/fz2017/dohvatiJSON.php?timezone=" + nazivVremenskeZone;
-        
-        //print(url);
-        
+        let url = server + "dohvatiJSON.php?timezone=" + nazivVremenskeZone;
         let adresa = URL(string: url)!
         let request = URLRequest(url: adresa)
-        
         let task = URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) -> Void in
-            guard let data = data, let json = (try? JSONSerialization.jsonObject(with: data, options: [])) as? [String: Any], let datum = json["datumISO8601"] as? String else {
+            guard let data = data, let json = (try? JSONSerialization.jsonObject(with: data, options: [])) as? [String: Any], let datum = json[self.formatDatumaJSON] as? String else {
                 return
             }
             DispatchQueue.main.async(execute: {
-                //print(datum)
                 let date = self.datumISO8601.date(from: datum)
                 self.datePicker.dateValue=date!
             })
         })
         task.resume()
-
-        
-        
     }
     
   
     
+    
+    
+    //https://code.bradymower.com/swift-3-apis-network-requests-json-getting-the-data-4aaae8a5efc0
     @IBAction func posalji(_ sender: Any) {
-        
-        //print("Šaljem")
-        
         let json: [String: Any] = ["timezone": nazivVremenskeZone,
                                    "datum": self.datumISO8601.string(from: self.datePicker.dateValue),
                                    "klijent": "swift"]
-        
         let jsonData = try? JSONSerialization.data(withJSONObject: json)
-
-        //print(datumISO8601.string(from: self.datePicker.dateValue))
-        
-        
-        let url = URL(string: "http://it.ffos.hr/fz2017/insertJSON.php")!
-
+        let url = URL(string: server + "insertJSON.php")!
         let request = NSMutableURLRequest(url: url)
         request.httpMethod = "POST"
         request.cachePolicy = NSURLRequest.CachePolicy.reloadIgnoringCacheData
         request.httpBody = jsonData
-        
         let task = URLSession.shared.dataTask(with: request as URLRequest) { (data, response, error) in
             guard let _: Data = data, let _: URLResponse = response, error == nil else {
-                print("*****error")
+                print("Greška")
                 return
             }
             let dataString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
-            print("*****Vratio server: \(String(describing: dataString))")
+            print("Vratio server: \(String(describing: dataString))")
         }
         task.resume()
-
-
-        
-            }
+    }
     
+    
+    
+    //poziva se prilikom otvaranja forme
     override func viewDidLoad() {
         let hr_HR = NSLocale(localeIdentifier: "hr_HR")
         self.datumISO8601 = DateFormatter()
